6cf7e7273f58144da4564095fd6792019d97570a
jiangzhenghui <970429796@qq.com>
2015-09-17 17:31:51 +0800

    add NewTest.java

---
+++ src/com/ninemax/test/NewTest.java
@@ -0,0 +1,10 @@
+package com.ninemax.test;
+
+/**
+ * Created by jiangzhenghui on 15/9/17.
+ */
+public class NewTest {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
